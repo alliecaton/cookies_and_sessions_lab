2b8475399d51347b06f7f3d9adc6feb2d8b7adb9
Allie <alliecaton3@gmail.com>
2021-03-02 13:10:41 -0500

    commit tree 348b1b6d9873811cbc54a41130a4075c7344e6d8

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+    def index
+    end 
+
+    def create
+        product = params[:product]
+        cart << product
+
+        render :index
+    end 
+
+
+end
